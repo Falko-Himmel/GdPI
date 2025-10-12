c59aaf04e9510b8341c7c75c3779a3ea6dab2977
Falko Leon Himmel <hi.falko@gmail.com>
2025-10-12 14:52:52 +0200

    verzweigungen beispiel

---
+++ verzweigungenHaskellJava/verzweigungen.hs
@@ -0,0 +1,32 @@
+--  in haskell gibt es verschiedene möglichkeiten verzweigungen zu machen
+--  die "einfachste" ist if then else
+
+signum :: Integer -> String
+signum x =
+  if x < 0
+    then "negativ"
+    else
+      if x == 0
+        then "null"
+        else "positiv"
+
+
+--  pattern matching mit guards (wächter) ist eine weitere möglichkeit verzweigungen zu machen
+
+signumGuards :: Integer -> String
+signumGuards x
+  | x < 0 = "negativ"
+  | x == 0 = "null"
+  | otherwise = "positiv"
+
+
+
+--  case of ist eine weitere möglichkeit verzweigungen zu machen
+
+signumCase :: Integer -> String
+signumCase x = case compare x 0 of
+  LT -> "negativ"
+  EQ -> "null"
+  GT -> "positiv"
+
+--  compare x 0 gibt LT, EQ oder GT zurück je nachdem ob x kleiner, gleich oder größer als 0 ist, ist bereits eine eingebaute funktion in haskell
