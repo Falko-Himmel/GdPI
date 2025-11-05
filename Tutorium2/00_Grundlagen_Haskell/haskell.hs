f63b8096203dc599730b5ee6aa3a0de2c093e11f
Falko Leon Himmel <hi.falko@gmail.com>
2025-11-05 12:47:46 +0100

    verschoben nach tut3

--- Tutorium2/00_Grundlagen_Haskell/haskell.hs
+++ Tutorium2/00_Grundlagen_Haskell/haskell.hs
@@ -2,21 +2,27 @@
 -- funktion besteht aus einem Bezeichner, Parametern und einem Ausdruck
 
 -- z.b.
-func x y = x + y
+add x y = x + y
+
+
 
 
 -- zustäzlich kann man optional eine Typsignatur hinzufügen
 
-func' :: Integer -> Integer -> Integer
-func' x y = x + y
+func' :: Integer -> Integer -> String -> Integer
+func' x y z = x + y
 
 -- Dabei Dateiname getrennt mit zwei Punkten und dann die Typen der Parameter in der richtigen Reihenfolge, der letzte Typ ist der Rückgabetyp
 
 
--- Aufgabe: Wir wollen die Zahlen 1 - 5 als Wort ausgeben, sonst ein Fehler
+-- Aufgabe: Wir wollen die Zahlen 1 - 3 als Wort ausgeben, sonst ein Fehler
 
 
-
+foo :: Integer -> Integer -> String
+foo 1 _= "eins"
+foo 2 2 = "zwei"
+foo 3 2= "drei"
+foo _ _= "fehler"
 
 
 
@@ -27,6 +33,8 @@ func' x y = x + y
 -- bisschen vorgegriffen eventuell, aber so können wir auch rekursion machen
 -- bei einem rekursiven algorithmus brauchen wir dabei immer einen Basisfall (mit diesem "bricht" die Rekursion ab)
 -- beispiel Fakultät: z.b. 6! = 6 * 5 * 4 * 3 * 2 * 1
+-- 5! = 5 * 4! = 5 * ( 4 * 3!) = 5 * ( 4 * (3 * (2!)))
+
 
 
 fak :: Integer -> Integer
@@ -35,8 +43,27 @@ fak x = x * fak (x-1)
 
 -- oder alternativ 
 
+fak' :: (Eq t, Num t) => t -> t -> t
 fak' 0 acc = acc
 fak' x acc = fak' (x-1) (x * acc)
 
+
+
 -- Frage was ist was? Welche Rekursionsart?
+
+foobar :: String -> String
+foobar x = x
+
+
+-- foob :: (Integer, Integer, Integer) -> (Integer, Integer, Integer) -> (Integer, Integer, Integer)
+
+-- alsdjhf
+-- aksdjfhkads
+
+
+-- kasdjhfa
+
+-- kasdjhk
+
+
 
