c7712cff4aa34320df86ee9560a90845e89d484e
Bayfrosch <bayfrosch@gmx.de>
2025-12-02 20:07:20 +0100

    feat(folds): :sparkles: fold.hs
    implemented few fold methods to display foldr and foldl

---
+++ Tutorium6/fold.hs
@@ -0,0 +1,46 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use map" #-}
+{-# HLINT ignore "Eta reduce" #-}
+{-# HLINT ignore "Use sum" #-}
+
+-- foldr und foldl sind Funktionen, die eine binäre Funktion, einen Anfangswert (Akkumulator) und eine Liste nehmen und die Funktion wiederholt anwenden, um einen einzigen Wert zu erzeugen.
+-- Der Unterschied zwischen foldr (fold right) und foldl (fold left) liegt in der Richtung, in der die Liste verarbeitet wird.
+-- foldr bildet einen rekursiven Term von rechts nach links, während foldl die Schritte "einzeln" von links nach rechts macht.
+
+-- Fold verhält sich manchmal gleich:
+foldSumr :: (Foldable t, Num b) => t b -> b
+foldSumr xs = foldr (+) 0 xs
+-- foldSumr [1,2,3,4,5] = 1+(2+(3+(4+(5+0)))) = 15
+
+foldSuml :: (Foldable t, Num b) => t b -> b
+foldSuml xs = foldl (+) 0 xs
+-- foldSuml [1,2,3,4,5] = ((((0+1)+2)+3)+4)+5 = 15
+
+
+-- Unterschied erkennbar bei division:
+
+foldDivr :: (Foldable t, Fractional b) => t b -> b
+foldDivr xs = foldr (/) 1 xs
+-- foldDivr [2,4,8] = 2/(4/(8/1)) = 4
+
+foldDivl :: (Foldable t, Fractional b) => t b -> b
+foldDivl xs = foldl (/) 1 xs
+-- foldDivl [2,4,8] = (((1/2)/4)/8) = 1/64
+
+
+-- Fold kann mit beliebigen Funktionen verwendet werden, die man auch selbst definieren kann:
+
+countEvens :: (Foldable t) => t Int -> Int
+countEvens xs = foldr (\acc x -> if even x then acc + 1 else acc) 0 xs
+-- countEvens [1,2,3,4,5,6] = 3
+
+
+-- Auf Fold basieren viele Listenfunktionen:
+
+lengthWithFoldr :: (Foldable t) => t a -> Int
+lengthWithFoldr xs = foldr (\_ acc -> acc + 1) 0 xs
+-- lengthWithFoldr [1,2,3,4,5] = 5
+
+mapWithFoldr :: (Foldable t) => (a -> b) -> t a -> [b]
+mapWithFoldr f xs = foldr (\x acc -> f x : acc) [] xs
+-- mapWithFoldr (*2) [1,2,3] = [2,4,6]
