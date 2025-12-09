7e976cb96946d7c2630528f38d81c23a3fc5df9b
Falko Leon Himmel <hi.falko@gmail.com>
2025-12-09 11:48:57 +0100

    del old sheet and renewed

---
+++ Tutorium7/sheet04.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+
+-- GdpI Blatt 4 -----------------------------------------------------------------
+-- Hinweis
+{-
+    Für die Aufgaben auf diesem Blatt sind die Funktionen `concat`, `foldr` und
+    `map`, Listenkomprehension, sowie die Funktionskomposition (.) nützliche
+    Werkzeuge.
+-}
+
+
+-- Aufgabe 1 --------------------------------------------------------------------
+-- a)
+-- Definition von Probe & Module
+
+-- b)
+calculateEnergyUsage :: undefined
+calculateEnergyUsage = undefined
+
+
+-- Aufgabe 2 --------------------------------------------------------------------
+-- a)
+-- Definition Dronen
+
+-- b) 
+-- Definition Schwarm
+
+-- c)
+-- Definition Developing
+
+-- d)
+-- Instanzdefinition: Dronen
+
+-- e)
+-- Instanzdefinition: Schwärme
+
+-- f)
+simulateDevelopment :: undefined
+simulateDevelopment = undefined
+
+-- g)
+accumulateOverTime :: undefined
+accumulateOverTime = undefined
